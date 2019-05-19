e6ab93c69a248c93f4d3956f30f1a0f8856eae3a
wannacry <chemaleonlugo@gmail.com>
2019-05-19 20:38:41 +0100

    fpm

--- wannacloud/script/ruby/ubuntu/manual/wannacloud.rb
+++ wannacloud/script/ruby/ubuntu/manual/wannacloud.rb
@@ -58,7 +58,7 @@ puts "[INFO] nginx ok"
 puts "[INFO] php checking..."
 
 ok = []
-ok << system("apt-get install -y php-imagick php7.2-common php7.2-gd php7.2-json php7.2-curl  php7.2-zip php7.2-xml php7.2-mbstring php7.2-bz2 php7.2-intl > /dev/null")
+ok << system("apt-get install -y php-fpm php-imagick php7.2-common php7.2-gd php7.2-json php7.2-curl  php7.2-zip php7.2-xml php7.2-mbstring php7.2-bz2 php7.2-intl > /dev/null")
 
 ok.each_with_index do |state, indecdx|
   if not state
