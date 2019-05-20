35e8b7cd7932aebc5bde7ea54e0c9590f60f75ef
Leon, Chema <chema.leon@firma.seznam.cz>
2019-05-20 11:31:07 +0200

    Update -q

--- wannacloud/script/ruby/debian/manual/enablepda.sh
+++ wannacloud/script/ruby/debian/manual/enablepda.sh
@@ -1,3 +1,3 @@
 apt install apt-transport-https lsb-release ca-certificates
-wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+wget -q -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
