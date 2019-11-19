2e81aa0825373436f7033d616531d0bce720fdb0
Wannaxry <chemaleonlugo@gmail.com>
2019-11-19 19:59:47 +0000

    actualizado a la 7.3

--- wannacloud/script/ruby/debian/auto/wannacloud.rb
+++ wannacloud/script/ruby/debian/auto/wannacloud.rb
@@ -64,7 +64,8 @@ puts "[INFO] nginx ok"
 puts "[INFO] php checking..."
 
 ok = []
-ok << system("apt-get install -y php7.0 php7.0-bz2 php7.0-cli php7.0-curl php7.0-fpm php7.0-gd php7.0-intl php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-opcache php7.0-sqlite3 php7.0-xml php7.0-zip php-apcu php-pear > /dev/null")
+ok << system("apt-get install -y php7.3-fpm php7.3-gd php7.3-mysql php7.3-curl php7.3-xml php7.3-zip php7.3-intl php7.3-mbstring php7.3-json php7.3-bz2 php7.3-ldap php-apcu imagemagick php-imagick php-smbclient -y > /dev/null")
+
 ok.each_with_index do |state, indecdx|
   if not state
     puts "[ERROR] php step #{index}!!!"
